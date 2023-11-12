7674e1b0d83b986003924547ed988208a7286ca5
Salvatore Ruocco <114869138+tatore02@users.noreply.github.com>
2023-11-12 01:04:43 +0100

    fileLoader singleton

---
+++ RazzoloDebug/src/it/unisa/razzolo_debug/LoadFile.java
@@ -0,0 +1,25 @@
+package it.unisa.razzolo_debug;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class LoadFile {
+
+    private LoadFile(){
+        final var url = getClass().getClassLoader().getResource("dictionary.txt");
+        assert url != null;
+        try {
+            file = new FileInputStream(url.getFile());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static FileInputStream getInstance(){
+        if (file == null)
+            new LoadFile();
+        return file;
+    }
+
+    private static FileInputStream file = null;
+}

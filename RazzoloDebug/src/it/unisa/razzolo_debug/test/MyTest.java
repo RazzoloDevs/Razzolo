821bb25eac141c31701446dce5f68346cce99349
Salvatore Ruocco <114869138+tatore02@users.noreply.github.com>
2023-12-11 00:35:50 +0100

    Update MyTest.java

--- RazzoloDebug/src/it/unisa/razzolo_debug/test/MyTest.java
+++ RazzoloDebug/src/it/unisa/razzolo_debug/test/MyTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -65,7 +68,7 @@ public class MyTest {
             for(int j=0;j<4;j++){
                 while(bufferedReader.ready()){
                     String line = bufferedReader.readLine();
-                    if(matrix[i][j] == line.charAt(0) && Algorithms.bfs(matrix, i, j, line))
+                    if(matrix[i][j] == line.charAt(0) && Algorithms.dfs(matrix, i, j, line))
                         foundWords.add(line);
                 }
                 file.getChannel().position(0);

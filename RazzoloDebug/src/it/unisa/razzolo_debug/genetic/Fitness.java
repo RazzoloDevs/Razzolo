ca116bd453f4ba56b1d080b98929c74070c26d28
Salvatore Ruocco <114869138+tatore02@users.noreply.github.com>
2023-12-11 01:31:53 +0100

    update genetic algorithm

--- RazzoloDebug/src/it/unisa/razzolo_debug/genetic/Fitness.java
+++ RazzoloDebug/src/it/unisa/razzolo_debug/genetic/Fitness.java
@@ -8,18 +8,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-/*
-    Gli individui sono caratterizzati da un array di Point
-*/
-
-public class Genetic {
-    /**
-     * @return numero di caratteri nell'ordine giusto
-     */
-    public static int fitness(final Point[] arrayPoint){
+public class Fitness {
+    public static int function(final Point[] arrayPoint){
         final var sb = new StringBuilder();
         for(Point p : arrayPoint)
-            sb.append(p);
+            sb.append(p.value());
 
         String s = sb.toString();
         int score = 0;
@@ -36,6 +29,9 @@ public class Genetic {
         return score;
     }
 
+    /**
+     * @return numero di caratteri nell'ordine giusto
+     */
     private static int evaluate(String s, String line){
         int score = 0;
         for(int i=0;i<s.length();i++){
--- RazzoloDebug/src/it/unisa/razzolo_debug/genetic/Selection.java
+++ RazzoloDebug/src/it/unisa/razzolo_debug/genetic/Selection.java
@@ -1,6 +1,13 @@
 package it.unisa.razzolo_debug.genetic;
 
+import it.unisa.razzolo_debug.model.Point;
+
+import java.util.List;
+
 public class Selection {
 
-    // Seleziono solo gli individui che hanno uno score > 0
+    // Seleziono solo gli individui che hanno un function > 0
+    public void fitnessGraterThan0(final List<Point[]> l){
+        l.removeIf(p -> Fitness.function(p) <= 0);
+    }
 }
